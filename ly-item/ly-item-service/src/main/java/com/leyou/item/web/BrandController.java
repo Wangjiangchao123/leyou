c971ac415399d765cee33c5eac293df5440e0134
Wangjiangchao123 <2396336636@qq.com>
2019-07-13 12:27:09 +0800

    first

--- ly-item/ly-item-service/src/main/java/com/leyou/item/web/BrandController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/web/BrandController.java
@@ -1,23 +1,23 @@
-//package com.leyou.item.web;
-//
-//import com.leyou.common.vo.PageResult;
-//import com.leyou.item.pojo.Brand;
-//import com.leyou.item.service.BrandService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("brand")
-//public class BrandController {
-//
-//    @Autowired
-//    private BrandService brandService;
-//
-//    public ResponseEntity<PageResult<Brand>> queryBrandPage(){
-//
-//        return  null;
-//    }
-//
-//}
+package com.leyou.item.web;
+
+import com.leyou.common.vo.PageResult;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("brand")
+public class BrandController {
+
+    @Autowired
+    private BrandService brandService;
+
+    public ResponseEntity<PageResult<Brand>> queryBrandPage(){
+
+        return  null;
+    }
+
+}
