27e5642b8307097c78e52eba05aa46c3858c42e0
wangjc <l>
2019-11-20 12:33:00 +0800

    swagger

--- ly-common/src/main/java/com/leyou/common/advice/CommomExceptionHandler.java
+++ ly-common/src/main/java/com/leyou/common/advice/CommomExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.leyou.common.advice;
 
-import com.leyou.common.enums.ExceptionEnums;
 import com.leyou.common.exception.LyException;
 import com.leyou.common.vo.ExceptionResult;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,7 +12,6 @@ public class CommomExceptionHandler {
     //拦截对象
     @ExceptionHandler(LyException.class)
     public ResponseEntity<ExceptionResult> handlerException(LyException e){
-//        ExceptionEnums enums = e.getExceptionEnums();
         return ResponseEntity.status(e.getExceptionEnums().getCode())
                 .body(new ExceptionResult(e.getExceptionEnums()));
     }
